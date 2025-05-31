603be19791b604eb28577c7daf7a3acac5a79b42
= <=>
2025-05-31 21:44:46 +0300

    implement py-shop-trip

--- app/main.py
+++ app/main.py
@@ -34,7 +34,8 @@ def shop_trip() -> None:
 
     shops: List[Shop] = []
     for s_data in shops_data:
-        shops.append(Shop(s_data["name"], s_data["location"], s_data["products"]))
+        shops.append(Shop(s_data["name"], s_data["location"],
+                          s_data["products"]))
 
     customers: List[Customer] = []
     for c_data in customers_data:
@@ -77,13 +78,15 @@ def shop_trip() -> None:
             all_shop_trip_costs_for_printing.append(
                 (shop.name, total_trip_cost_for_printing))
 
-            if can_fulfill_cart and customer.money >= total_trip_cost_for_printing:
+            if (can_fulfill_cart and customer.money
+                    >= total_trip_cost_for_printing):
                 if total_trip_cost_for_printing < cheapest_total_trip_cost:
                     cheapest_total_trip_cost = total_trip_cost_for_printing
                     best_shop_for_customer = shop
 
         for shop_name, cost in all_shop_trip_costs_for_printing:
-            print(f"{customer.name}'s trip to the {shop_name} costs {cost:.2f}")
+            print(f"{customer.name}'s trip to the "
+                  f"{shop_name} costs {cost:.2f}")
 
         if best_shop_for_customer is None:
             print(f"{customer.name} doesn't have enough money to make a "
@@ -107,13 +110,14 @@ def shop_trip() -> None:
             cost_item = price * quantity
             receipt_products_total_cost += cost_item
 
-            formatted_item_cost = (int(cost_item) if cost_item == int(cost_item)
+            formatted_item_cost = (int(cost_item)
+                                   if cost_item == int(cost_item)
                                    else cost_item)
             print(f"{quantity} {product}s for {formatted_item_cost} dollars")
 
         formatted_receipt_total = (int(receipt_products_total_cost) if
-                                   receipt_products_total_cost == \
-                                   int(receipt_products_total_cost) else
+                                   receipt_products_total_cost
+                                   == int(receipt_products_total_cost) else
                                    receipt_products_total_cost)
         print(f"Total cost is {formatted_receipt_total} dollars")
         print("See you again!")
@@ -129,7 +133,8 @@ def shop_trip() -> None:
                 distance_for_fuel_deduction) * 2
         fuel_cost_deduction = fuel_liters_deduction * fuel_price
 
-        total_money_spent_on_trip = receipt_products_total_cost + fuel_cost_deduction
+        total_money_spent_on_trip = (receipt_products_total_cost
+                                     + fuel_cost_deduction)
         customer.money -= total_money_spent_on_trip
 
         print(f"{customer.name} now has {customer.money:.2f} dollars")
